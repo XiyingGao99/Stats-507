0a1c34d2cf16b0f658bd3929df2b178d4ad8a6bf
xiying <89849944+XiyingGao99@users.noreply.github.com>
2021-11-12 18:47:35 -0500

    Add pd_topic_group2_4c_xiying.py

---
+++ pandas_notes/pd_topic_group2_4c_xiying.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_json: true
+#     notebook_metadata_filter: markdown
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+#   
+
+# ## Contents
+# Add a bullet for each topic and link to the level 2 title header using 
+# the exact title with spaces replaced by a dash. 
+#
+# + [Table Styler](#Table Styler) 
+
+
+# ## Table Styler
+# ### Manipulate many parameters of a table using the table Styler object in pandas. *Xiying Gao*
